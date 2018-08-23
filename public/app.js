a2715c8e43ebfb958539022791b406da89090462
Joshua Arnett <Josharnett850@gmail.com>
2018-08-23 00:01:35 -0400

    completed all client side logic

--- public/app.js
+++ public/app.js
@@ -8,15 +8,16 @@ $(document).ready(function() {
         }
     });
 
+    $(".list").on("click", "li", function() {
+        updateTodo($(this));
+    });
+
     // listening for click on list after span is loaded.
-    $(".list").on("click", "span", function() {
-        console.log($(this).parent().data("id"));
-        // $(this).parent().remove();
-        // $.ajax({
-        //     method: "DELETE",
-        //     url: "/api/todos/"
-        // })
-    })
+    $('.list').on('click', 'span', function(e){
+        // Stops the event from bubbling up 
+        e.stopPropagation();
+        removeTodo($(this).parent());
+    });
 });
 
 
@@ -30,6 +31,7 @@ const addTodos = function(todos) {
 const addTodo = function(todo) {
     let newTodo = $("<li class='task'>" + todo.name + "<span>X</span></li>");
     newTodo.data("id", todo._id);
+    newTodo.data("completed", todo.completed);
     if(todo.completed) {
         newTodo.addClass("done");
     }
@@ -45,5 +47,37 @@ const createTodo = function() {
     })
     .catch(function(err) {
         console.log(err);
+    })
+}
+
+const removeTodo = function(todo){
+    var clickedId = todo.data('id');
+    var deleteUrl = '/api/todos/' + clickedId; 
+    $.ajax({
+      method: 'DELETE',
+      url: deleteUrl
+    })
+    .then(function(data){
+        console.log(data);
+        todo.remove();
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+}
+
+const updateTodo = function(todo) {
+    let updateUrl = '/api/todos/' + todo.data("id");
+    let isDone = !todo.data("completed");
+    let updateData = {completed: isDone}
+    console.log(updateData);
+    $.ajax({
+        method: "PUT",
+        url: updateUrl,
+        data: updateData
+    })
+    .then(function(updatedTodo) {
+        todo.toggleClass("done");
+        todo.data("completed", isDone);
     })
 }
